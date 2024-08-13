6bbce0027048f83eab7fc8de05c637036c859585
Gjermund Skobba <gjermund@skobba.net>
2024-08-13 17:03:33 +0200

    Deploy to skobba.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://skobba.net'
+})
---
+++ src/content/docs/git/README.md
@@ -0,0 +1,7 @@
+---
+title: index
+---
+
+# index
+
+This is docs/git/index.md
